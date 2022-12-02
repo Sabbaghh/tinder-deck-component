ad2be26e8dec2df0fa916fff6f18caee2159372e
Sabbaghh <your_email@example.com>
2022-12-02 12:31:24 +0400

    initail

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Deck from './src/components/Deck';
+import { DATA } from './dummyData';
+import { Card, Button, Icon } from '@rneui/themed';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from 'react';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const renderCard = (item) => {
+		const { text, uri } = item;
+		return (
+			<Card>
+				<Card.Image
+					resizeMode='cover'
+					source={{
+						uri: uri,
+					}}
+				/>
+				<Card.Divider />
+				<Card.Title>{text}</Card.Title>
+				<Button
+					icon={
+						<Icon name='code' color='#ffffff' iconStyle={{ marginRight: 10 }} />
+					}
+					buttonStyle={{
+						borderRadius: 0,
+						marginLeft: 0,
+						marginRight: 0,
+						marginBottom: 0,
+					}}
+					title='VIEW NOW'
+				/>
+			</Card>
+		);
+	};
+	return (
+		<>
+			<SafeAreaView />
+			<SafeAreaProvider>
+				<Deck DATA={DATA} renderCard={renderCard} />
+				<StatusBar style='auto' />
+			</SafeAreaProvider>
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
