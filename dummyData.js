cc48f91094a9367488c8bc1a8686e3ea6547c233
Sabbaghh <your_email@example.com>
2022-12-02 12:31:48 +0400

    Adding dummy data

---
+++ dummyData.js
@@ -0,0 +1,42 @@
+export const DATA = [
+	{
+		id: 1,
+		text: 'Card #1',
+		uri: 'https://picsum.photos/200/300',
+	},
+	{
+		id: 2,
+		text: 'Card #2',
+		uri: 'https://picsum.photos/200/300',
+	},
+	{
+		id: 3,
+		text: 'Card #3',
+		uri: 'https://picsum.photos/200/300',
+	},
+	{
+		id: 4,
+		text: 'Card #4',
+		uri: 'https://picsum.photos/200/300',
+	},
+	{
+		id: 5,
+		text: 'Card #5',
+		uri: 'https://picsum.photos/200/300',
+	},
+	{
+		id: 6,
+		text: 'Card #6',
+		uri: 'https://picsum.photos/200/300',
+	},
+	{
+		id: 7,
+		text: 'Card #7',
+		uri: 'https://picsum.photos/200/300',
+	},
+	{
+		id: 8,
+		text: 'Card #8',
+		uri: 'https://picsum.photos/200/300',
+	},
+];
